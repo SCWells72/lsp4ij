018f85c41f030a9b89cce380fde4c6d264048eb8
Scott Wells <scott@illuminatedcloud.com>
2025-01-08 12:56:24 -0600

    Going to break up the different on-type triggers into distinct test classes.

--- src/test/java/com/redhat/devtools/lsp4ij/features/formatting/TypeScriptClientSideFormatOnCloseBraceTest.java
+++ src/test/java/com/redhat/devtools/lsp4ij/features/formatting/TypeScriptClientSideFormatOnCloseBraceTest.java
@@ -15,20 +15,20 @@ import com.redhat.devtools.lsp4ij.client.features.LSPFormattingFeature.Formattin
 import com.redhat.devtools.lsp4ij.fixtures.LSPClientSideOnTypeFormattingFixtureTestCase;
 
 /**
- * TypeScript-based client-side on-type formatting tests.
+ * TypeScript-based client-side on-type formatting tests for format-on-close brace.
  */
-public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTypeFormattingFixtureTestCase {
+public class TypeScriptClientSideFormatOnCloseBraceTest extends LSPClientSideOnTypeFormattingFixtureTestCase {
 
     private static final String TEST_FILE_NAME = "test.ts";
 
-    public TypeScriptClientSideOnTypeFormattingTest() {
+    public TypeScriptClientSideFormatOnCloseBraceTest() {
         super("*.ts");
     }
 
-    // SIMPLE FORMAT-ON-CLOSE-BRACE TESTS
+    // SIMPLE TESTS
 
     // language=json
-    private static final String SIMPLE_FOCB_MOCK_SELECTION_RANGE_JSON = """
+    private static final String SIMPLE_MOCK_SELECTION_RANGE_JSON = """
             [
               {
                 "range": {
@@ -70,7 +70,7 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
             """;
 
     // language=json
-    private static final String SIMPLE_FOCB_MOCK_FOLDING_RANGE_JSON = """
+    private static final String SIMPLE_MOCK_FOLDING_RANGE_JSON = """
             [
               {
                 "startLine": 0,
@@ -84,7 +84,7 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
             """;
 
     // language=json
-    private static final String SIMPLE_FOCB_MOCK_RANGE_FORMATTING_JSON = """
+    private static final String SIMPLE_MOCK_RANGE_FORMATTING_JSON = """
             [
               {
                 "range": {
@@ -103,7 +103,7 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
             """;
 
     // No language injection here because there are syntax errors
-    private static final String SIMPLE_FOCB_FILE_BODY_BEFORE = """
+    private static final String SIMPLE_FILE_BODY_BEFORE = """
             export class Foo {
                 bar() {
             console.log('Hello, world.');
@@ -111,10 +111,10 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
             }
             """;
 
-    public void testSimpleFormatOnCloseBraceDefaults() {
+    public void testSimpleDefaults() {
         assertOnTypeFormatting(
                 TEST_FILE_NAME,
-                SIMPLE_FOCB_FILE_BODY_BEFORE,
+                SIMPLE_FILE_BODY_BEFORE,
                 // language=typescript
                 """
                         export class Foo {
@@ -123,17 +123,17 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
                             }
                         }
                         """,
-                SIMPLE_FOCB_MOCK_SELECTION_RANGE_JSON,
-                SIMPLE_FOCB_MOCK_FOLDING_RANGE_JSON,
-                SIMPLE_FOCB_MOCK_RANGE_FORMATTING_JSON,
+                SIMPLE_MOCK_SELECTION_RANGE_JSON,
+                SIMPLE_MOCK_FOLDING_RANGE_JSON,
+                SIMPLE_MOCK_RANGE_FORMATTING_JSON,
                 null // No-op as the default is disabled
         );
     }
 
-    public void testSimpleFormatOnCloseBraceEnabled() {
+    public void testSimpleEnabled() {
         assertOnTypeFormatting(
                 TEST_FILE_NAME,
-                SIMPLE_FOCB_FILE_BODY_BEFORE,
+                SIMPLE_FILE_BODY_BEFORE,
                 // language=typescript
                 """
                         export class Foo {
@@ -142,17 +142,17 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
                             }
                         }
                         """,
-                SIMPLE_FOCB_MOCK_SELECTION_RANGE_JSON,
-                SIMPLE_FOCB_MOCK_FOLDING_RANGE_JSON,
-                SIMPLE_FOCB_MOCK_RANGE_FORMATTING_JSON,
+                SIMPLE_MOCK_SELECTION_RANGE_JSON,
+                SIMPLE_MOCK_FOLDING_RANGE_JSON,
+                SIMPLE_MOCK_RANGE_FORMATTING_JSON,
                 clientConfiguration -> clientConfiguration.format.formatOnCloseBrace = true
         );
     }
 
-    public void testSimpleFormatOnCloseBraceEnabledNoCurlyBrace() {
+    public void testSimpleEnabledNoCurlyBrace() {
         assertOnTypeFormatting(
                 TEST_FILE_NAME,
-                SIMPLE_FOCB_FILE_BODY_BEFORE,
+                SIMPLE_FILE_BODY_BEFORE,
                 // language=typescript
                 """
                         export class Foo {
@@ -161,9 +161,9 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
                             }
                         }
                         """,
-                SIMPLE_FOCB_MOCK_SELECTION_RANGE_JSON,
-                SIMPLE_FOCB_MOCK_FOLDING_RANGE_JSON,
-                SIMPLE_FOCB_MOCK_RANGE_FORMATTING_JSON,
+                SIMPLE_MOCK_SELECTION_RANGE_JSON,
+                SIMPLE_MOCK_FOLDING_RANGE_JSON,
+                SIMPLE_MOCK_RANGE_FORMATTING_JSON,
                 clientConfiguration -> {
                     clientConfiguration.format.formatOnCloseBrace = true;
                     // Explicitly specify close brace characters that don't include right curly brace
@@ -172,10 +172,10 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
         );
     }
 
-    // COMPLEX FORMAT-ON-CLOSE-BRACE TESTS
+    // COMPLEX TESTS
 
     // language=json
-    private static final String COMPLEX_FOCB_MOCK_SELECTION_RANGE_JSON = """
+    private static final String COMPLEX_MOCK_SELECTION_RANGE_JSON = """
             [
               {
                 "range": {
@@ -205,7 +205,7 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
             """;
 
     // language=json
-    private static final String COMPLEX_FOCB_MOCK_FOLDING_RANGE_JSON = """
+    private static final String COMPLEX_MOCK_FOLDING_RANGE_JSON = """
             [
               {
                 "startLine": 0,
@@ -219,7 +219,7 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
             """;
 
     // language=json
-    private static final String COMPLEX_FOCB_MOCK_RANGE_FORMATTING_JSON = """
+    private static final String COMPLEX_MOCK_RANGE_FORMATTING_JSON = """
             [
               {
                 "range": {
@@ -264,7 +264,7 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
             """;
 
     // No language injection here because there are syntax errors
-    private static final String COMPLEX_FOCB_FILE_BODY_BEFORE = """
+    private static final String COMPLEX_FILE_BODY_BEFORE = """
             export class Foo {
             bar() {
             console.log('Hello, world.');
@@ -272,10 +272,10 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
             // type }
             """;
 
-    public void testComplexFormatOnCloseBraceDefaults() {
+    public void testComplexDefaults() {
         assertOnTypeFormatting(
                 TEST_FILE_NAME,
-                COMPLEX_FOCB_FILE_BODY_BEFORE,
+                COMPLEX_FILE_BODY_BEFORE,
                 // language=typescript
                 """
                         export class Foo {
@@ -284,17 +284,17 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
                         }
                         }
                         """,
-                COMPLEX_FOCB_MOCK_SELECTION_RANGE_JSON,
-                COMPLEX_FOCB_MOCK_FOLDING_RANGE_JSON,
-                COMPLEX_FOCB_MOCK_RANGE_FORMATTING_JSON,
+                COMPLEX_MOCK_SELECTION_RANGE_JSON,
+                COMPLEX_MOCK_FOLDING_RANGE_JSON,
+                COMPLEX_MOCK_RANGE_FORMATTING_JSON,
                 null // No-op as the default is disabled
         );
     }
 
-    public void testComplexFormatOnCloseBraceEnabled() {
+    public void testComplexEnabled() {
         assertOnTypeFormatting(
                 TEST_FILE_NAME,
-                COMPLEX_FOCB_FILE_BODY_BEFORE,
+                COMPLEX_FILE_BODY_BEFORE,
                 // language=typescript
                 """
                         export class Foo {
@@ -303,17 +303,17 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
                             }
                         }
                         """,
-                COMPLEX_FOCB_MOCK_SELECTION_RANGE_JSON,
-                COMPLEX_FOCB_MOCK_FOLDING_RANGE_JSON,
-                COMPLEX_FOCB_MOCK_RANGE_FORMATTING_JSON,
+                COMPLEX_MOCK_SELECTION_RANGE_JSON,
+                COMPLEX_MOCK_FOLDING_RANGE_JSON,
+                COMPLEX_MOCK_RANGE_FORMATTING_JSON,
                 clientConfiguration -> clientConfiguration.format.formatOnCloseBrace = true
         );
     }
 
-    public void testComplexFormatOnCloseBraceEnabledNoCurlyBrace() {
+    public void testComplexEnabledNoCurlyBrace() {
         assertOnTypeFormatting(
                 TEST_FILE_NAME,
-                COMPLEX_FOCB_FILE_BODY_BEFORE,
+                COMPLEX_FILE_BODY_BEFORE,
                 // language=typescript
                 """
                         export class Foo {
@@ -322,9 +322,9 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
                         }
                         }
                         """,
-                COMPLEX_FOCB_MOCK_SELECTION_RANGE_JSON,
-                COMPLEX_FOCB_MOCK_FOLDING_RANGE_JSON,
-                COMPLEX_FOCB_MOCK_RANGE_FORMATTING_JSON,
+                COMPLEX_MOCK_SELECTION_RANGE_JSON,
+                COMPLEX_MOCK_FOLDING_RANGE_JSON,
+                COMPLEX_MOCK_RANGE_FORMATTING_JSON,
                 clientConfiguration -> {
                     clientConfiguration.format.formatOnCloseBrace = true;
                     // Explicitly specify close brace characters that don't include right curly brace
@@ -333,9 +333,9 @@ public class TypeScriptClientSideOnTypeFormattingTest extends LSPClientSideOnTyp
         );
     }
 
-    // FORMAT-ON-CLOSE-BRACE SCOPE TESTS
+    // SCOPE TESTS
 
-    public void testFormatOnCloseBraceEnabledFileScope() {
+    public void testEnabledFileScope() {
         // language=json
         String mockRangeFormattingJson = """
                 [
